c7288864af41ed2727640ecfd271fead1b5dadaf
KeeganBruer <keegan.bruer@gmail.com>
2019-11-15 10:24:32 -0500

    Update Graph.java

--- src/Graph.java
+++ src/Graph.java
@@ -9,7 +9,8 @@ import java.util.ArrayList;
 public class Graph implements GraphInterface {
 	
 	ArrayList<Vertex> vertices = new ArrayList<Vertex>();
-	
+	Queue<Vertex> bfsQueue = new LinkedList<>();
+
 	public Graph() {
 		//
 	}
